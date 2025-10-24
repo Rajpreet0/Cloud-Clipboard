f4a35d890a745de22b30febc899eb5e5a4f179e7
Rajpreet0 <Rajpreet.Singh@gmx.de>
2025-10-24 16:24:43 +0200

    added ui in settings and refactored heartbeat

--- cloud-clipboard-desktop-app/src/components/SettingsItem.tsx
+++ cloud-clipboard-desktop-app/src/components/SettingsItem.tsx
@@ -15,7 +15,9 @@ const SettingsItem: React.FC<SettingsItemProps> =  ({
       <div className="flex items-center justify-between w-full mt-2">
           <div className="text-left">
                   <h3 className="text-md text-gray-500">{itemTitle}</h3>
-                  <p className="text-sm text-gray-400 mt-2">{itemDescription}</p>
+                  {itemDescription && (
+                    <p className="text-sm text-gray-400 mt-2">{itemDescription}</p>
+                  )}
           </div>
           {/* Can be different Items */}
           {children}
