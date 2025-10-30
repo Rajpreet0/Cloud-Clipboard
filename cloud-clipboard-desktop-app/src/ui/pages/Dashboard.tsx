a2688f5a30eab4551c23e91c27809927dffdc8d9
Rajpreet0 <rajpreet.singh@gmx.de>
2025-10-31 00:01:14 +0100

    added search Feature and Timestamp for Clips

--- cloud-clipboard-desktop-app/src/ui/pages/Dashboard.tsx
+++ cloud-clipboard-desktop-app/src/ui/pages/Dashboard.tsx
@@ -1,29 +1,46 @@
 import ClipItem from "@/components/ClipItem";
 import Layout from "@/components/Layout";
 import { useAuth } from "@/context/AuthContext";
+import { formatTimestamp } from "@/lib/formatTime";
 import { CircleCheck, Laptop, RefreshCcw, SearchIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 
-type ClipItem = {
+type ClipItemType = {
   type:"text"|"image";
   data:string;
+  timestamp: number;
 };
 
 const Dashboard = () => {
 
     const { auth, loading } = useAuth();
-    const [clips, setClips] = useState<ClipItem[]>([]);
+    const [clips, setClips] = useState<ClipItemType[]>([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-      const listener = window.clips.onNew((payload: ClipItem) => {
-        setClips(prev => [payload, ...prev]);
+      const listener = window.clips.onNew((payload: Omit<ClipItemType, "timestamp">) => {
+        const newClip: ClipItemType = {
+          ...payload,
+          timestamp: Date.now(),
+        };
+        setClips((prev) => [newClip, ...prev]);
       });
       return () => window.clips.offNew(listener);
     }, []);
 
     if (loading) return <p>Loading...</p>;
     if (!auth) return null;
+
+    // Filter Clips
+    const filteredClips = useMemo(() => {
+      const term = search.toLowerCase();
+      return clips.filter((clip) => 
+        clip.type === "text"
+          ? clip.data.toLowerCase().includes(term)
+          : term === ""
+      );
+    }, [clips, search]);
 
 
   return (
@@ -41,20 +58,28 @@ const Dashboard = () => {
         <input
           type="text"
           placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           className="w-full bg-transparent text-sm text-dark-gray placeholder-gray-400 outline-none"
         />
       </div>
       <div className="mt-4 flex flex-col gap-2">
-        {clips.map((clip, idx) => (
+        {filteredClips.length > 0 ? (
+          filteredClips.map((clip, idx) => (
           <ClipItem
             key={idx}
             type={clip.type}
             data={clip.data}
             icon={Laptop}
             deviceType="MacBook Air"
-            clipTimeAgo="2 min ago"
+            clipTimestamp={formatTimestamp(clip.timestamp)}
           />
-        ))}
+        ))
+        ) : (
+          <p className="text-gray-400 text-sm mt-4 text-center">
+            No matching clips found.
+          </p>
+        )}
       </div>
     </Layout>
   )
